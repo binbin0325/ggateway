ca6c90090af36e2e18827b5933ca64264ceb773e
binbin.zhang <binbin.zhang@openjawtech.com>
2020-06-05 10:48:32 +0800

    TT-000 commit

--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.14
 
 require (
 	github.com/fsnotify/fsnotify v1.4.7
+	github.com/nacos-group/nacos-sdk-go v0.3.2
 	github.com/panjf2000/gnet v1.2.0
 	github.com/spf13/viper v1.7.0
-	github.com/valyala/fasthttp v1.13.1
-	go.uber.org/zap v1.10.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
---
+++ pkg/cc/nacos/nacos.go
@@ -0,0 +1,93 @@
+//Alibaba Nacos config center
+package nacos
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/spf13/viper"
+
+	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/clients/nacos_client"
+	"github.com/nacos-group/nacos-sdk-go/common/http_agent"
+
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+)
+
+var clientConfigTest = constant.ClientConfig{
+	TimeoutMs:           10 * 1000,
+	BeatInterval:        5 * 1000,
+	ListenInterval:      300 * 1000,
+	NotLoadCacheAtStart: true,
+	//Username:            "nacos",
+	//Password:            "nacos",
+}
+var once sync.Once
+var configClient config_client.ConfigClient
+
+func GetConfigClient() config_client.ConfigClient {
+	//实现单例
+	once.Do(func() {
+		configClient = initConfigClientTest()
+	})
+	return configClient
+
+}
+
+func initConfigClientTest() config_client.ConfigClient {
+	nc := nacos_client.NacosClient{}
+	nc.SetServerConfig([]constant.ServerConfig{constant.ServerConfig{
+		IpAddr:      viper.GetString("nacos.config.ip"),
+		Port:        viper.GetUint64("nacos.config.port"),
+		ContextPath: "/nacos",
+	}})
+	nc.SetClientConfig(clientConfigTest)
+	nc.SetHttpAgent(&http_agent.HttpAgent{})
+	client, _ := config_client.NewConfigClient(&nc)
+	return client
+}
+
+func main() {
+	client := initConfigClientTest()
+	content, _ := client.GetConfig(vo.ConfigParam{
+		DataId: "dataId",
+		Group:  "group",
+	})
+	fmt.Println("config :" + content)
+	_, err := client.PublishConfig(vo.ConfigParam{
+		DataId:  "dataId",
+		Group:   "group",
+		Content: "hello world!"})
+	if err != nil {
+		fmt.Printf("success err:%s", err.Error())
+	}
+	content = ""
+
+	client.ListenConfig(vo.ConfigParam{
+		DataId: "dataId",
+		Group:  "group",
+		OnChange: func(namespace, group, dataId, data string) {
+			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", data:" + data)
+			content = data
+		},
+	})
+
+	client.ListenConfig(vo.ConfigParam{
+		DataId: "abc",
+		Group:  "DEFAULT_GROUP",
+		OnChange: func(namespace, group, dataId, data string) {
+			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", data:" + data)
+		},
+	})
+
+	time.Sleep(5 * time.Second)
+	_, err = client.PublishConfig(vo.ConfigParam{
+		DataId:  "dataId",
+		Group:   "group",
+		Content: "abc"})
+
+	select {}
+
+}
